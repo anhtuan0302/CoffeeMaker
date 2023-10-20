acd1d6c6bc0b392ad3d948ca435352e9ad7a85da
anhtuan0302 <anhtuan030203@gmail.com>
2023-10-21 04:20:18 +0700

    Coffee Maker Console

--- Coffee Maker/Client.cs
+++ Coffee Maker/Client.cs
@@ -15,7 +15,13 @@ namespace Coffee_Maker
             while (true)
             {
                 Console.WriteLine("1. Make Espresso");
+                Console.WriteLine("2. Make Americano");
+                Console.WriteLine("3. Make Cappuccino");
+                Console.WriteLine("4. Make Latte");
+                Console.WriteLine("5. Make Hot Water");
+                Console.WriteLine("6. Make Clean");
                 Console.WriteLine("0. Exit");
+                Console.WriteLine("------------------------------");
                 Console.Write("Please enter your choice: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch(choice)
@@ -24,15 +30,16 @@ namespace Coffee_Maker
                         Console.WriteLine("==============================");
                         Console.WriteLine("1. Default");
                         Console.WriteLine("2. Custom");
+                        Console.WriteLine("------------------------------");
                         Console.Write("Choose an option: ");
-                        int option = Convert.ToInt32(Console.ReadLine());
-                        if (option == 1)
+                        int espressoOption = Convert.ToInt32(Console.ReadLine());
+                        if (espressoOption == 1)
                         {
                             Coffee espresso = coffeeMachine.MakeEspresso(false);
                             Console.WriteLine("Your espresso is ready!");
                             espresso.Result();
                         }
-                        else if (option == 2)
+                        else if (espressoOption == 2)
                         {
                             Coffee espresso = coffeeMachine.MakeEspresso(true);
                             Console.WriteLine("Your espresso is ready!");
@@ -42,6 +49,110 @@ namespace Coffee_Maker
                         {
                             Console.WriteLine("Invalid choice!");
                         }
+                        break;
+                    case 2:
+                        Console.WriteLine("==============================");
+                        Console.WriteLine("1. Default");
+                        Console.WriteLine("2. Custom");
+                        Console.WriteLine("------------------------------");
+                        Console.Write("Choose an option: ");
+                        int AmericanoOption = Convert.ToInt32(Console.ReadLine());
+                        if (AmericanoOption == 1)
+                        {
+                            Coffee americano = coffeeMachine.MakeAmericano(false);
+                            Console.WriteLine("Your americano is ready!");
+                            americano.Result();
+                        }
+                        else if (AmericanoOption == 2)
+                        {
+                            Coffee americano = coffeeMachine.MakeAmericano(true);
+                            Console.WriteLine("Your americano is ready!");
+                            americano.Result();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice!");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("==============================");
+                        Console.WriteLine("1. Default");
+                        Console.WriteLine("2. Custom");
+                        Console.WriteLine("------------------------------");
+                        Console.Write("Choose an option: ");
+                        int CappuccinoOption = Convert.ToInt32(Console.ReadLine());
+                        if (CappuccinoOption == 1)
+                        {
+                            Coffee capuccino = coffeeMachine.MakeCappuccino(false);
+                            Console.WriteLine("Your cappuccino is ready!");
+                            capuccino.Result();
+                        }
+                        else if (CappuccinoOption == 2)
+                        {
+                            Coffee cappuccino = coffeeMachine.MakeCappuccino(true);
+                            Console.WriteLine("Your cappuccino is ready!");
+                            cappuccino.Result();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice!");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("==============================");
+                        Console.WriteLine("1. Default");
+                        Console.WriteLine("2. Custom");
+                        Console.WriteLine("------------------------------");
+                        Console.Write("Choose an option: ");
+                        int LatteOption = Convert.ToInt32(Console.ReadLine());
+                        if (LatteOption == 1)
+                        {
+                            Coffee latte = coffeeMachine.MakeLatte(false);
+                            Console.WriteLine("Your latte is ready!");
+                            latte.Result();
+                        }
+                        else if (LatteOption == 2)
+                        {
+                            Coffee latte = coffeeMachine.MakeAmericano(true);
+                            Console.WriteLine("Your latte is ready!");
+                            latte.Result();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice!");
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("==============================");
+                        Console.WriteLine("1. Default");
+                        Console.WriteLine("2. Custom");
+                        Console.WriteLine("------------------------------");
+                        Console.Write("Choose an option: ");
+                        int hotWaterOption = Convert.ToInt32(Console.ReadLine());
+                        string hotWaterResult;
+                        if (hotWaterOption == 1)
+                        {
+                            hotWaterResult = coffeeMachine.MakeHotWater(false);
+                            Console.WriteLine(hotWaterResult);
+                            Console.WriteLine("==============================");
+                        }
+                        else if (hotWaterOption == 2)
+                        {
+                            hotWaterResult = coffeeMachine.MakeHotWater(true);
+                            Console.WriteLine(hotWaterResult);
+                            Console.WriteLine("==============================");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice!");
+                        }
+                        break;
+                    case 6:
+                        string cleanResult;
+                        Console.WriteLine("==============================");
+                        cleanResult = coffeeMachine.MakeClean();
+                        Console.WriteLine(cleanResult);
+                        Console.WriteLine("==============================");
                         break;
                     case 0:
                         Console.WriteLine("Exiting...");
--- Coffee Maker/CoffeeMachineFacade.cs
+++ Coffee Maker/CoffeeMachineFacade.cs
@@ -57,5 +57,154 @@ namespace Coffee_Maker
             Console.WriteLine("==============================");
             return new Coffee(customCoffeePowderAmount, customWaterAmount, customMilkAmount, customFrothMilkAmount, 90);
         }
+
+        public Coffee MakeAmericano(bool custom)
+        {
+            customMilkAmount = 0;
+            customFrothMilkAmount = 0;
+            if (custom)
+            {
+                Console.Write("Enter coffee powder amount (g): ");
+                int coffeePowderAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter water amount (ml): ");
+                int waterAmount = Convert.ToInt32(Console.ReadLine());
+
+                customCoffeePowderAmount = coffeePowderAmount;
+                customWaterAmount = waterAmount;
+            }
+            else
+            {
+                customCoffeePowderAmount = 5;
+                customWaterAmount = 35;
+            }
+
+            Console.WriteLine("==============================");
+            pump.SetWater(customWaterAmount);
+            pump.PumpWater();
+            heater.SetTemp(90);
+            heater.Heat();
+            grinder.SetGrindAmount(customCoffeePowderAmount);
+            grinder.Grind();
+            Console.WriteLine("==============================");
+            return new Coffee(customCoffeePowderAmount, customWaterAmount, customMilkAmount, customFrothMilkAmount, 90);
+        }
+
+        public Coffee MakeCappuccino(bool custom)
+        {
+            if (custom)
+            {
+                Console.Write("Enter coffee powder amount (g): ");
+                int coffeePowderAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter water amount (ml): ");
+                int waterAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter milk amount (ml): ");
+                int milkAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter froth milk amount (ml): ");
+                int frothMilkAmount = Convert.ToInt32(Console.ReadLine());
+
+                customCoffeePowderAmount = coffeePowderAmount;
+                customWaterAmount = waterAmount;
+                customMilkAmount = milkAmount;
+                customFrothMilkAmount = frothMilkAmount;
+            }
+            else
+            {
+                customCoffeePowderAmount = 5;
+                customWaterAmount = 10;
+                customMilkAmount = 10;
+                customFrothMilkAmount = 15;
+            }
+
+            Console.WriteLine("==============================");
+            pump.SetWater(customWaterAmount);
+            pump.PumpWater();
+            heater.SetTemp(90);
+            heater.Heat();
+            grinder.SetGrindAmount(customCoffeePowderAmount);
+            grinder.Grind();
+            pump.SetMilk(customMilkAmount);
+            pump.PumpMilk();
+            heater.SetTemp(50);
+            heater.Heat();
+            milkFrother.SetMilk(customFrothMilkAmount);
+            milkFrother.FrothMilk();
+            Console.WriteLine("==============================");
+            return new Coffee(customCoffeePowderAmount, customWaterAmount, customMilkAmount, customFrothMilkAmount, 90);
+        }
+
+        public Coffee MakeLatte(bool custom)
+        {
+            if (custom)
+            {
+                Console.Write("Enter coffee powder amount (g): ");
+                int coffeePowderAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter water amount (ml): ");
+                int waterAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter milk amount (ml): ");
+                int milkAmount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter froth milk amount (ml): ");
+                int frothMilkAmount = Convert.ToInt32(Console.ReadLine());
+
+                customCoffeePowderAmount = coffeePowderAmount;
+                customWaterAmount = waterAmount;
+                customMilkAmount = milkAmount;
+                customFrothMilkAmount = frothMilkAmount;
+            }
+            else
+            {
+                customCoffeePowderAmount = 5;
+                customWaterAmount = 10;
+                customMilkAmount = 15;
+                customFrothMilkAmount = 10;
+            }
+
+            Console.WriteLine("==============================");
+            pump.SetWater(customWaterAmount);
+            pump.PumpWater();
+            heater.SetTemp(90);
+            heater.Heat();
+            grinder.SetGrindAmount(customCoffeePowderAmount);
+            grinder.Grind();
+            pump.SetMilk(customMilkAmount);
+            pump.PumpMilk();
+            heater.SetTemp(50);
+            heater.Heat();
+            milkFrother.SetMilk(customFrothMilkAmount);
+            milkFrother.FrothMilk();
+            Console.WriteLine("==============================");
+            return new Coffee(customCoffeePowderAmount, customWaterAmount, customMilkAmount, customFrothMilkAmount, 90);
+        }
+
+        public string MakeHotWater(bool custom)
+        {
+            if (custom)
+            {
+                Console.Write("Enter water amount (ml): ");
+                int waterAmount = Convert.ToInt32(Console.ReadLine());
+
+                customWaterAmount = waterAmount;
+            }
+            else
+            {
+                customWaterAmount = 100;
+            }
+
+            Console.WriteLine("==============================");
+            pump.SetWater(customWaterAmount);
+            pump.PumpWater();
+            heater.SetTemp(100);
+            heater.Heat();
+            return "Hot water " + customWaterAmount  + "ml is ready!";
+        }
+
+        public string MakeClean()
+        {            
+            Console.WriteLine("==============================");
+            pump.SetWater(100);
+            pump.PumpWater();
+            heater.SetTemp(100);
+            heater.Heat();
+            return "Cleaning is done!";
+        }
     }
 }
